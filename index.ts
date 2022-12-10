d1d7b1300f9ebbbb31baef53a44921260f93b63d
tararoshan <troshan@utexas.edu>
2022-12-10 15:03:53 -0600

    Add support for higher WPMs
    Via an async function and while loop (lines 40 - 60). However,
    
    * the WPM calculation is still inaccurate and
    * puppetter will continue to type after reaching the end screen.
    
    These issues still need to be fixed.

--- index.ts
+++ index.ts
@@ -1,20 +1,15 @@
 import puppeteer from "puppeteer";
 
-/* Final WPM, feel free to change it to your liking. */
-const WORDS_PER_MIN = 107;
-/* Assuming it's 30 words by default. */
-const NUM_WORDS = 30;
+/* Final WPM, feel free to change it to your liking! */
+const WORDS_PER_MIN = 400;
+/* For the 30-second typing test (default), there are 100 words to type. */
+const NUM_WORDS = 100;
 const SECONDS_PER_MIN = 60;
 const MILLISECONDS_PER_SECONDS = 1_000;
 
-const TOTAL_MILLISECONDS = NUM_WORDS / WORDS_PER_MIN * SECONDS_PER_MIN * MILLISECONDS_PER_SECONDS;
-console.log(TOTAL_MILLISECONDS);
-
-/* Also assuming that the app will only let you type for 30 seconds. I do this
-   because the app will stop allowing input after a while of stable WPM, so
-   before you wouldn't be able to see the results screen for a while until all
-   of the original words are typed. */
-const MAX_SECONDS = 30;
+/* TODO this calculation is still wrong LOL */
+const TOTAL_MILLISECONDS = NUM_WORDS * MILLISECONDS_PER_SECONDS * SECONDS_PER_MIN / WORDS_PER_MIN;
+console.log('total milliseconds ' + TOTAL_MILLISECONDS);
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -30,18 +25,42 @@ const MAX_SECONDS = 30;
   /* Click "Reject All" cookies button. */
   await page.click('.rejectAll');
   
-  const FULL_TEXT = await page.$eval('#words', el => el.innerText);
+  /* Get the first batch of 100 words to type. */
+  const FULL_TEXT = await page.$eval('#words', elem => elem.innerText);
 
+  // FOR DEBUGGING
   const OUTPUT = FULL_TEXT.replaceAll('\n', ' ');
-  console.log(`outputting (${OUTPUT})`);
+  console.log(`outputting\n (${OUTPUT})\n length: ${OUTPUT.length}`);
+  var futurePattern = OUTPUT.substring(OUTPUT.length - 30, OUTPUT.length);
 
   const MILLISECONDS_PER_LETTER = TOTAL_MILLISECONDS / OUTPUT.length;
-  console.log(MILLISECONDS_PER_LETTER);
-  const FINAL_LEN = MAX_SECONDS * MILLISECONDS_PER_SECONDS / MILLISECONDS_PER_LETTER;
-  console.log(FINAL_LEN);
+  console.log('milliseconds per letter ' + MILLISECONDS_PER_LETTER);
 
-  await page.type('#words', OUTPUT, {delay: MILLISECONDS_PER_LETTER});
-  console.log("done")
+  /* Wait for a second so that we don't start typing too fast for monkeytype. */
+  setTimeout(async () => {
+    await page.type('#words', OUTPUT, {delay: MILLISECONDS_PER_LETTER});
+    console.log("done typing first set of words");
+    /* Figure out the next set of words, if needed. The WPM may cause more words to appear after the
+       initial 100. */
+    var curText;
+    while (curText = await page.$eval('#words', elem => elem.innerText)) {
+      console.log('detected more text to type');
+      curText = curText.replaceAll('\n', ' ');
+      await console.log(`current text: (${curText})`);
+      var startLen = curText.search(futurePattern) + futurePattern.length;
+      console.log(`new input: (${curText.substring(startLen, curText.length)})`)
+      await page.type('#words',
+                curText.replaceAll('\n', ' ').substring(startLen, curText.length),
+                {delay: MILLISECONDS_PER_LETTER}
+      );
+      await console.log('finished typing another set.');
+      startLen = await curText.length;
+      futurePattern = curText.substring(curText.length - 30, curText.length);
+    }
+  }, MILLISECONDS_PER_SECONDS);
+
+  /* TODO there's still the issue of after 30 seconds, we're still typing. Is there a function that
+     can stop puppetteer from typing? I could do a setTimeout() for that. */
+  
+  console.log('base calculations done.');
 })();
-
-// TODO run out of words after a certain length/count
