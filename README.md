3ee6d42e4be580b9ed4ec6f57057849d6fdd5acc
tararoshan <troshan@utexas.edu>
2022-12-19 17:56:43 -0800

    Clean code and comments in index.ts, populate README
    Removed most debugging messages, added more comments in index.ts in
    case I choose to redo or review this project later on. Added tips
    on how to get started on recreating the project in the readme for
    others (in particular, AWMies) to make their own typing bot.
    
    A persistent issue is that the WPM is consistently off, which I tried
    to remedy with line 43 in index.ts. The difference is more noticable
    in higher WPMs (eg. 600), so it can be removed for more realistic
    typing speeds like 100 WPM for better bot accuracy.
    
    I'm satisfied with what I've learned and made with this project, so
    this project is done for now. Hooray! :)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # monkeytype-bot
-bot that lets you have whatever wpm on monkeytype.com
+A bot that lets you have whatever wpm on [monkeytype.com](monkeytype.com)!
+
+## To run
+To run, type `npm run typetest`, the shortcut I defined in package.json for `npx ts-node index.ts`.
+
+## To recreate
+To recreate this project, use [Puppeteer](https://pptr.dev/) and inspect the webpage of the typing
+test you'd like to mess with. I'd recommend starting the test and then typing `debugger` into the
+console of the inspector, which pauses the page. From there, you can look at the inner text of the
+elements on the page using `document.querySelector()`, etc. This is what `page.$()` does in
+Puppeteer! For more information about Puppeteer's functions, check out their
+[guide](https://pptr.dev/) and feel free to reference my comments & code in index.ts! Have fun!
--- index.ts
+++ index.ts
@@ -8,11 +8,8 @@ const MILLISECONDS_PER_MIN = MILLISECONDS_PER_SECOND * SECONDS_PER_MIN;
 /* For the 30-second typing test (default), there are 100 words to type. */
 const NUM_WORDS = 100;
 
-/* Final WPM, feel free to change it to your liking! */
-const USER_WPM = 230; // CHANGE THIS VALUE!!
-
-/* TODO this calculation is still wrong LOL */
-console.log('total milliseconds ' + TOTAL_MILLISECONDS);
+/* Final WPM, feel free to change it to your liking! It'll be a little off. */
+const USER_WPM = 100; // CHANGE THIS VALUE!! :)
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -33,41 +30,42 @@ console.log('total milliseconds ' + TOTAL_MILLISECONDS);
   curText = curText.replaceAll('\n', ' ');
 
   const WORDS_PER_CHAR = NUM_WORDS / curText.length;
-  console.log('words per char ' + WORDS_PER_CHAR);
+  console.log('words per char: ' + WORDS_PER_CHAR);
+  /* The number of characters we should aim to type to get the desired WPM. */
   const TARGET_NUM_CHAR = (TOTAL_MILLISECONDS * USER_WPM) / (MILLISECONDS_PER_MIN * WORDS_PER_CHAR); 
-  console.log('target num char ' + TARGET_NUM_CHAR);
+  console.log('target num char: ' + TARGET_NUM_CHAR);
+  /* Get the delay for each character. */
   var MILLISECONDS_PER_LETTER = TOTAL_MILLISECONDS / TARGET_NUM_CHAR;
-  console.log('milliseconds per letter ' + MILLISECONDS_PER_LETTER);
-  MILLISECONDS_PER_LETTER /= 1.5;
-
-  console.log(`outputting\n (${curText})\n length: ${curText.length}`);
-  var futurePattern = curText.substring(curText.length - 30, curText.length);
-  
+  console.log('milliseconds per letter: ' + MILLISECONDS_PER_LETTER);
+  /* Reduce the delay by a bit -- I've noticed that the WPM is off, even with all of these
+     calculations. Reader, please let me know if you see an issue here, but I haven't yet figured
+     out why the WPM is consistently below the target WPM, even after re-doing my math. Thanks! */
+  MILLISECONDS_PER_LETTER /= 1.3;
+  console.log(`\noutputting the following:\n (${curText})\n length: ${curText.length}`);
+  /* In case we need to type the next batch of words, figure out where we left off from (the
+     starting words for each batch changes. The choice of 15 is arbitrary. */
+  var futurePattern = curText.substring(curText.length - 15, curText.length);
   
   /* Wait for a second so that we don't start typing too fast for monkeytype. */
   setTimeout(async () => {
     let charIndex: number = 0;
     
     while (await page.$eval('#miniTimerAndLiveWpm', elem => elem.children[0].innerHTML != '0')) {
-      // type a character
       await page.type('#words', curText.charAt(charIndex), {delay: MILLISECONDS_PER_LETTER});
+      /* Uncomment below for debugging, otherwise spams the console. */
       // console.log(`typing ${curText.charAt(charIndex)}, which is at index ${charIndex}`);
       charIndex++;
-      // figure out if we need the next batch of words
+      /* Figure out if we need the next batch of words. */
       if (charIndex == curText.length) {
-        console.log('need to get next batch of words!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-        console.log(`old text: (${curText})\n`);
         curText = await page.$eval('#words', elem => elem.innerText);
-        console.log(`number of words: ${curText.split('\n').length}`)
         curText = curText.replaceAll('\n', ' ');
-        console.log(`new text: (${curText})\n with length ${curText.length}`)
+        console.log(`\nnew text: \n (${curText})\n length: ${curText.length}`);
+        console.log(`number of words: ${curText.split(' ').length}`);
         charIndex = curText.search(futurePattern) + futurePattern.length;
-        futurePattern = curText.substring(curText.length - 30, curText.length);
-        console.log(`cur word: ${curText.substring(charIndex, charIndex + 6)}`);
+        // console.log(`cur word: ${curText.substring(charIndex, charIndex + 6)}`);  // debugging
+        /* Setting up for next batch, if needed. */
+        futurePattern = curText.substring(curText.length - 15, curText.length);
       }
-      console.log(charIndex);
     }
   }, MILLISECONDS_PER_SECOND);
-
-  console.log('base calculations done.');
 })();
